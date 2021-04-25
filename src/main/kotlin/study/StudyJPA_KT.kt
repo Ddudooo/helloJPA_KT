2105aece22d6481b3776cbf7a52cd67500aff409
Ddudooo <igoa.work@gmail.com>
2021-04-25 22:17:09 +0900

    JPA 영속성 컨텍스트 공부

--- src/main/kotlin/study/StudyJPA_KT.kt
+++ src/main/kotlin/study/StudyJPA_KT.kt
@@ -12,21 +12,20 @@ fun main(){
     val tx = em.transaction
     tx.begin()
     try {
+        //비영속
         val member :Member = Member(1L, "홍길동")
-        /*val member : Member = em.find(Member::class.java, 1L);
-        member.name = "수정"*/
-        //수정시 persist 필요없음.
-
-        //쿼리 작성
-        /*
-        val resultList = em.createQuery(
-            "select m from Member as m",
-            Member::class.java)
-            .setFirstResult(1)
-            .setMaxResults(100)
-            .resultList
-        */
+        //영속
+        println("=== BEFORE ===")
+        //1차 캐시에 저장.
         em.persist(member)
+        println("=== AFTER ===")
+
+        //1차 캐시에서 조회
+        val findMember = em.find(Member::class.java, 1L)
+        println(findMember)
+
+        //커밋 시점에 db 쿼리 실행
+        //트랜잭션 커밋 시점에서 DB 요청을 보내기때문에 일종의 쓰기 지연식으로 사용가능하다.
         tx.commit()
     } catch (e: Exception){
         tx.rollback()
