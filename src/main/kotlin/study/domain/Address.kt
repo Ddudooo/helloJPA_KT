593f00b5d1c0ca752f72258e601040efd16fd61e
Ddudooo <igoa.work@gmail.com>
2021-05-04 23:13:06 +0900

    JPA 프록시와 연관관계 관리
    값 타입의 동등성 비교

--- src/main/kotlin/study/domain/Address.kt
+++ src/main/kotlin/study/domain/Address.kt
@@ -15,4 +15,26 @@ class Address(
     ): Address {
         return Address(city, street, zipcode)
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Address
+
+        if (city != other.city) return false
+        if (street != other.street) return false
+        if (zipcode != other.zipcode) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = city.hashCode()
+        result = 31 * result + street.hashCode()
+        result = 31 * result + zipcode.hashCode()
+        return result
+    }
+
+
 }
